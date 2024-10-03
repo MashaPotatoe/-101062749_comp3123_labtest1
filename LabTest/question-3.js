54075b7970d842df4a3c3e97f1588ddee6839b24
MashaPotatoe <masha.sofro98@gmail.com>
2024-10-03 18:32:05 -0400

    finished

--- LabTest/question-3.js
+++ LabTest/question-3.js
@@ -14,10 +14,10 @@ if(!fs.existsSync(logDir)) {
     fs.mkdirSync(logDir);
 }
 //create the 10 files :) i know its not the best but i did them 1 at a time  
-//currentlog = fs.appendFile('./logs/mylog9.txt', 'log number 9 in the directory', function (error){
- //   if (error) throw error;
-  //  console.log(`log was saved!`)
-//})
+currentlog = fs.appendFile('./logs/mylog9.txt', 'log number 9 in the directory', function (error){
+    if (error) throw error;
+    console.log(`log was saved!`)
+})
 
 //const readLog = fs.readdirSync(logDir);
 //console.log('The logs are these ones :D', readLog)
@@ -25,20 +25,27 @@ if(!fs.existsSync(logDir)) {
 
 //fs.open('mylog1.txt', 'r');
 
-currentLog = fs.appendFile('./logs/mylog5.txt', 'log number 5', (error) => {
-    if (error) throw error;
+currentLog = fs.appendFile('./logs/mylog10.txt', 'log number 5', (error) => {
+  if (error) throw error;
     console.log('log was saved!');
 });
 
-//removingLog = fs.rmSync('./logs/mylog5.txt')
+const readLog = fs.readdirSync(logDir);
+console.log('The logs are these ones', readLog)
 
+
+// how i think the removing works 
+removingLog = fs.rmSync('./logs/mylog5.txt')
+
+// i wasnt able to get the foreach to work
 fs.readdirSync(logDir, function (error, logs) {
     if (error){
         console.log('theres an error')
     }
     logs.logDir.forEach(logs => { 
-        const removeLogs = fs.rmSync('./logDir');
+        const removeLogs = fs.rmSync(logDir);
         console.log(`Deleted ${logs}`);
     });
 })
 
+fs.rmdirSync(logDir)
